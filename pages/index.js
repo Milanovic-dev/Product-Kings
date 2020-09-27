8c0f84d16ba54b75cb5b8d514263c3fbbb7a0b3c
nikolamilanovic21 <nikolamilanovic21@gmail.com>
2020-09-27 13:32:29 +0200

    added Eslint

--- pages/index.js
+++ pages/index.js
@@ -1,31 +1,15 @@
-import { EmptyState,Page, Heading, Card, ResourceList, ResourceItem, TextStyle } from '@shopify/polaris';
-import { TitleBar } from '@shopify/app-bridge-react';
-import dbConnect  from '../database/dbConnect';
-import { useState, useEffect } from 'react'
+import { EmptyState,Page } from '@shopify/polaris';
+import { useState } from 'react'
 import { useRouter } from 'next/router';
-import { empty } from 'apollo-boost';
-import gql from 'graphql-tag';
-import { Query } from 'react-apollo';
-
-//dbConnect();
-
-const GET_SHOP = gql`
-query {
-  shop {
-    name
-    storefrontUrl
-    url
-  }
-}`
 
 const img = 'https://cdn.shopify.com/s/files/1/0757/9955/files/empty-state.svg';
 
 const Index = () => {
+  // eslint-disable-next-line no-unused-vars
   const [emptyState, setEmptyState] = useState(true);
   return (
-    <Page fullWidth={true}>    
-        {emptyState ? <Landing/> : null
-        }  
+    <Page fullWidth>    
+      {emptyState ? <Landing /> : null}  
     </Page>
   )
 }
@@ -33,13 +17,13 @@ const Index = () => {
 const Landing = () => {
   const router = useRouter();
   return (
-  <EmptyState
-    heading="Create your own customizable product builder"
-    action={{ content: 'Get Started', onAction: () => router.push('/products') }}
-    image={img}
-  >
-    <p>Secure, simple and blazing fast.</p>
-  </EmptyState>
+    <EmptyState
+      heading="Create your own customizable product builder"
+      action={{ content: 'Get Started', onAction: () => router.push('/products') }}
+      image={img}
+    >
+      <p>Secure, simple and blazing fast.</p>
+    </EmptyState>
   );
 }
 
